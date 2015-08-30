1ca7c49dc3c7728ad83b55c85d5c185aee5ddb01
Mizhal <mizhal@gmail.com>
2015-08-30 20:28:34 +0200

    ssh: nombres de directorio mas representativos, actualizacion del proceso de copia de claves, documentacion

--- setup-keys.sh
+++ setup-keys.sh
@@ -2,7 +2,8 @@
 KEYDIR=~/.ssh/keys
 rm $KEYDIR -rf
 mkdir $KEYDIR
-cp /media/vagrant_shared/keys/private/* $KEYDIR -R
-chmod go-rwx $KEYDIR/* -R
-cp /media/vagrant_shared/keys/config ~/.ssh
-echo "ssh-add ~/.ssh/keys/*" >> ~/.bashrc
+cp /media/vagrant_shared/ssh/keys/* $KEYDIR -R
+chmod go-rwx $KEYDIR -R
+cp /media/vagrant_shared/ssh/config ~/.ssh
+## Anyadir a sesion las identidades (archivos que no tengan extension pub)
+echo 'ssh-add `find ~/.ssh/keys | grep -v ".*[.]pub"`' >> ~/.bashrc
